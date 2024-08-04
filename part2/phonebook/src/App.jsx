8f6d5d0c8f35cd954f59ad5896c5fee7448c3a16
SchneiderSeba <seba_19_sc@hotmail.com>
2024-08-04 16:53:03 +0200

    part2 phonebook-useEffect

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,28 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Persons } from './Persons'
 import { Form } from './Form'
 import { Filter } from './Filter'
+import axios from 'axios'
+
 
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {setPersons(response.data)})
+  }, [])
+
+  const [persons, setPersons] = useState([]) 
 
   const [newName, setNewName] = useState('debug')
 
   const [newNumber, setNewNumber] = useState('debug')
 
   const [search, setSearch] = useState('')
+
 
   const handleChangeName = (e) => {
     setNewName(e.target.value)
@@ -42,7 +46,6 @@ const handleSubmit = (e) => {
     setNewName('')
     setNewNumber('')
 }
-
 const handleSearch = (e) => {
   setSearch(e.target.value)
 }
