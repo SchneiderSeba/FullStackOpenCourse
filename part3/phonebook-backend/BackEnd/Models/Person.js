e7c16454051644c97b23908286d85474b17bfcc4
SchneiderSeba <seba_19_sc@hotmail.com>
2024-09-15 19:54:46 +0200

    validations

--- part3/phonebook-backend/BackEnd/Models/Person.js
+++ part3/phonebook-backend/BackEnd/Models/Person.js
@@ -3,7 +3,17 @@ import mongoose from 'mongoose'
 const personSchema = new mongoose.Schema({
   id: Number,
   name: { type: String, required: true, minLength: 4 },
-  number: { type: String, required: true, minLength: 8 }
+  number: {
+    type: String,
+    required: true,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /\d{3}-\d{6}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  }
 })
 
 const Person = mongoose.model('Person', personSchema)
