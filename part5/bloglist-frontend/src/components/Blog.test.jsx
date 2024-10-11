f21845e97afd09ce3f7c8e007e0b478745aeb2ad
SchneiderSeba <seba_19_sc@hotmail.com>
2024-10-11 12:21:49 +0200

    test btn like

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,18 +1,35 @@
 import { render, screen } from '@testing-library/react'
 import { Blog } from './Blog'
-import { expect, test } from 'vitest'
+import { expect, test, vi } from 'vitest'
+import userEvent from '@testing-library/user-event'
 
-test('renders blog title', () => {
+test('renders blog title', async () => {
   const blog = {
     title: 'Test blog',
     author: 'Test author',
-    url: 'Test url'
+    url: 'Test url',
+    likes: 0
   }
 
-  render(<Blog blog={blog} />)
+  const mockHandler = vi.fn()
+  const mockUpdateHandler = vi.fn().mockResolvedValue({ likes: 1 })
+
+  render(<Blog blog={blog} onClick={mockHandler} handleUpdateBlog={mockUpdateHandler}/>)
+
+  const user = userEvent.setup()
+  const buttonView = screen.getByRole('button', { name: /view/i })
+  expect(buttonView).toBeInTheDocument()
+  await user.click(buttonView)
+
+  const buttonLike = screen.getByText('Like 👍')
+  await user.click(buttonLike)
+
+  screen.debug()
 
   const element = screen.getByText('Test blog')
 
   expect(element).toBeInTheDocument()
+
+  expect(mockUpdateHandler).toHaveBeenCalledTimes(1)
 })
 
