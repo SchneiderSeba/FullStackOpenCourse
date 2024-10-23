f6c813255a367ee881b186bd304e5f347600628d
SchneiderSeba <seba_19_sc@hotmail.com>
2024-10-23 11:39:39 +0200

    5.16 done

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -4,9 +4,9 @@ import { FormNewBlog } from './BlogSection'
 import { beforeEach, describe, expect, test, vi } from 'vitest'
 import userEvent from '@testing-library/user-event'
 
-describe('Blog component', () => {
+describe('Blog component 5.13 > 5.15', () => {
 
-  test('Render component Blog Close showing Title 5.13', async () => {
+  test('5.13 Render component Blog Close showing Title', async () => {
     const blog = {
       title: 'Test blog',
       author: 'Test author',
@@ -29,7 +29,7 @@ describe('Blog component', () => {
 
   })
 
-  test('Render Component Blog Open showing likes & Link 5.14', async () => {
+  test('5.14 Render Component Blog Open showing likes & Link', async () => {
     const blog = {
       title: 'Test blog',
       author: 'Test author',
@@ -60,7 +60,7 @@ describe('Blog component', () => {
 
     const element = screen.getByText('Test blog')
 
-    screen.debug()
+    // screen.debug()
 
     expect(link).toBeInTheDocument()
 
@@ -71,6 +71,62 @@ describe('Blog component', () => {
     expect(mockUpdateHandler).toHaveBeenCalledTimes(1)
   })
 
+  test('5.15 Twice Click on Like is Called Twice', async () => {
+    const blog = {
+      title: 'Test blog',
+      author: 'Test author',
+      url: 'Test url',
+      likes: 0,
+      user: {
+        id: 'testId'
+      }
+    }
+
+    const mockHandler = vi.fn()
+    const mockUpdateHandler = vi.fn().mockResolvedValue({ likes: 1 })
+
+    const user = userEvent.setup({ user: { id : 'testId' } })
+
+    render(<Blog blog={blog} onClick={mockHandler} handleUpdateBlog={mockUpdateHandler} user={user}/>)
+
+    const buttonView = screen.getByRole('button', { name: /view/i })
+
+    await user.click(buttonView)
+
+    const buttonLike = screen.getByText('Like 👍')
+    await user.dblClick(buttonLike)
+
+    expect(mockUpdateHandler).toHaveBeenCalledTimes(2)
+
+  })
+})
+
+describe('5.16 Form check', () => {
+
+  test('<FormNewBlog /> updates parent state and calls onSubmit', async () => {
+    const handleCreateBlog = vi.fn()
+    const user = userEvent.setup({ user: { id : ' testId' } })
+
+    const component = render(<FormNewBlog handleCreateBlog={handleCreateBlog} />)
+
+    // screen.debug()
+
+    const inputTitle = component.getByPlaceholderText('Title')
+    const inputAuthor = component.getByPlaceholderText('Author')
+    const inputUrl = component.getByPlaceholderText('URL')
+
+    await userEvent.type(inputTitle, 'Test title')
+    await userEvent.type(inputAuthor, 'Test author')
+    await userEvent.type(inputUrl, 'Test url')
+    await userEvent.click(component.getByRole('button', { name: 'Create' }))
+
+    expect(handleCreateBlog).toHaveBeenCalledTimes(1)
+    expect(handleCreateBlog.mock.calls[0][0]).toEqual({
+      title: 'Test title',
+      author: 'Test author',
+      url: 'Test url'
+    })
+  })
 })
 
 describe('ToggleFunction', () => {
@@ -105,33 +161,5 @@ describe('ToggleFunction', () => {
 
     const div = container.querySelector('.blogContent')
     expect(div).not.toBeNull()
-  })
-})
-
-describe('Form check', () => {
-
-  test('<FormNewBlog /> updates parent state and calls onSubmit', async () => {
-    const handleCreateBlog = vi.fn()
-    const user = userEvent.setup({ user: { id : ' testId' } })
-
-    const component = render(<FormNewBlog handleCreateBlog={handleCreateBlog} />)
-
-    // screen.debug()
-
-    const inputTitle = component.getByPlaceholderText('Title')
-    const inputAuthor = component.getByPlaceholderText('Author')
-    const inputUrl = component.getByPlaceholderText('URL')
-
-    await userEvent.type(inputTitle, 'Test title')
-    await userEvent.type(inputAuthor, 'Test author')
-    await userEvent.type(inputUrl, 'Test url')
-    await userEvent.click(component.getByRole('button', { name: 'Create' }))
-
-    expect(handleCreateBlog).toHaveBeenCalledTimes(1)
-    expect(handleCreateBlog.mock.calls[0][0]).toEqual({
-      title: 'Test title',
-      author: 'Test author',
-      url: 'Test url'
-    })
   })
 })
