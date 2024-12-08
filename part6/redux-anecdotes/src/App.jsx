dd2ecf09543688b12d59cdb7141f1c6c047779cf
SchneiderSeba <seba_19_sc@hotmail.com>
2024-12-08 18:33:06 +0000

    6.3, 6.4, 6.5, 6.6, 6.7, 6.8 done

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,27 +1,19 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { createAnecdote } from './reducers/anecdoteReducer.js'
+import { useSelector } from 'react-redux'
 import { createStore } from 'redux'
 import { useState } from 'react'
 import { reducer } from './reducers/anecdoteReducer.js'
 import { AnecdotesList } from './components/AnecdotesList.jsx'
+import { AnecdotesForm } from './components/AnecdotesForm.jsx'
 
 const store = createStore(reducer)
 
 const App = () => {
   const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
 
   const [refresh, setRefresh] = useState(false)
 
   const refreshPage = () => {
     setRefresh(!refresh)
-  }
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
   }
 
   store.subscribe(refreshPage)
@@ -31,10 +23,7 @@ const App = () => {
       <h2>Anecdotes</h2>
       <AnecdotesList anecdotes={anecdotes} />
       <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name='anecdote'/></div>
-        <button type='submite'>create➕</button>
-      </form>
+      <AnecdotesForm />
     </div>
   )
 }
---
+++ part6/redux-anecdotes/src/components/AnecdotesForm.jsx
@@ -0,0 +1,18 @@
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+
+export const AnecdotesForm = () => {
+    const dispatch = useDispatch()
+    const addAnecdote = (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        event.target.anecdote.value = ''
+        dispatch(createAnecdote(content))
+    }
+    return (
+        <form onSubmit={addAnecdote}>
+          <input name="anecdote" />
+          <button type="submit">add</button>
+        </form>
+    )
+}
